e14cfde4a2595a60ce40e00e14c1fd4d9abf071f
Stefan Pantic <stefanpantic13@gmail.com>
2018-01-04 22:18:10 +0100

    Added signum function, reformating

--- include/rgdefines.hpp
+++ include/rgdefines.hpp
@@ -1,4 +1,22 @@
 #pragma once
 
-constexpr float pi{3.14159265359};
+namespace eRG::util
+{
+
+	/* Constant expression for PI */
+	constexpr float pi{3.14159265359};
+
+	/*
+	* @brief Typesafe signum function.
+	*
+	* @param T NumericType type.
+	*/
+	template<typename T>
+	constexpr int sgn(T val)
+	{
+		return (T(0) < val) - (T(0) > val);
+	}
+
+}; /* namespace eRG::util */
+
 
