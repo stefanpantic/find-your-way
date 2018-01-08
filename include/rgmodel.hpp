b9eb22ed4435a743bf3fe88b05fa6bfa6fb05617
Stefan Pantic <stefanpantic13@gmail.com>
2018-01-08 09:38:31 +0100

    Redesigned eRG::Model abstract class

--- include/rgmodel.hpp
+++ include/rgmodel.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
+#include <iostream>
 #include <glm/vec3.hpp>
-#include <glm/vec4.hpp>
 
 namespace eRG
 {
@@ -9,40 +9,32 @@ namespace eRG
 	/* Model declaration: */
 	/* @{ */
 	/*
-	* @brief Model interface.
+	* @brief Model abstract class.
 	*/
 	class Model
 	{
+		/* Convenience */
+		protected:
+			using v3cr = const glm::vec3&;
+
 		/* Public member functions */
 		public:
 			/* Construction */
-			explicit Model( glm::vec3 translate = {0, 0, 0},
-							glm::vec3 scale		= {1, 1, 1},
-							glm::vec4 rotate 	= {0, 0, 0, 0});
+			explicit Model( glm::vec3 lower_left_near = {0, 0, 0},
+							glm::vec3 upper_right_far = {1, 1, 1});
 			explicit Model(const Model &other);
 			explicit Model(Model &&other);
 
-			/* Relative transformations */
-			void translate_r(glm::vec3 translate);
-			void scale_r(glm::vec3 scale);
-			void rotate_r(glm::vec4 rotate);
-
-			/* Absolute transformations */
-			void translate_a(glm::vec3 translate);
-			void scale_a(glm::vec3 scale);
-			void rotate_a(glm::vec4 rotate);
-
 			/* Position */
-			virtual const glm::vec3& position() = 0;	
+			virtual std::pair<v3cr, v3cr> position() = 0;
 
 			/* Draw */
 			virtual void draw() = 0;
 		/* Protected member variables */
 		protected:
-			/* Transformations */
-			glm::vec3 translate_;
-			glm::vec3 scale_;
-			glm::vec4 rotate_;
+			/* Stored corners */
+			glm::vec3 lln_;
+			glm::vec3 urf_;
 	}; /* class eRG::Model */
 	/* @} */
 
--- src/rgmodel.cpp
+++ src/rgmodel.cpp
@@ -11,14 +11,11 @@ namespace eRG
 	/* Construction: */
 	/* @{ */
 	/*
-	* @brief Builds instance of %eRG::Model.
+	* @brief Builds %eRG::Model.
 	*/
-	Model::Model(	glm::vec3 translate,
-					glm::vec3 scale,
-					glm::vec4 rotate)
-		:	translate_{translate},
-			scale_{scale},
-			rotate_{rotate}
+	Model::Model(	glm::vec3 lower_left_near,
+					glm::vec3 upper_right_far)
+		:	lln_{lower_left_near}, urf_{upper_right_far}
 	{
 		std::clog << "eRG::Model: Default constructor" << std::endl;
 	}
@@ -27,9 +24,7 @@ namespace eRG
 	* @brief Copy constructor.
 	*/
 	Model::Model(const Model &other)
-		:	translate_{other.translate_},
-			scale_{other.scale_},
-			rotate_{other.rotate_}
+		:	lln_{other.lln_}, urf_{other.urf_}
 	{
 		std::clog << "eRG::Model: Copy constructor" << std::endl;
 	}
@@ -38,65 +33,9 @@ namespace eRG
 	* @brief Move constructor.
 	*/
 	Model::Model(Model &&other)
-		:	translate_{std::move(other.translate_)},
-			scale_{std::move(other.scale_)},
-			rotate_{std::move(other.rotate_)}
+		:	lln_{std::move(other.lln_)}, urf_{std::move(other.urf_)}
 	{
 		std::clog << "eRG::Model: Move constructor" << std::endl;
-	}
-	/* @} */
-
-	/* Relative transformations: */
-	/* @{ */
-	/*
-	* @brief Translate model relative to stored transformation.
-	*/
-	void Model::translate_r(glm::vec3 translate)
-	{
-		translate_ += std::move(translate);
-	}
-
-	/*
-	* @brief Scale model relative to stored transformation.
-	*/
-	void Model::scale_r(glm::vec3 scale)
-	{
-		scale_ += std::move(scale);
-	}
-
-	/*
-	* @brief Rotate model relative to stored transformation.
-	*/
-	void Model::rotate_r(glm::vec4 rotate)
-	{
-		rotate_ += std::move(rotate);
-	}
-	/* @} */
-
-	/* Absolute transformations: */
-	/* @{ */
-	/*
-	* @brief Set model translation.
-	*/
-	void Model::translate_a(glm::vec3 translate)
-	{
-		translate_ = std::move(translate);
-	}
-
-	/*
-	* @brief Set model scale.
-	*/
-	void Model::scale_a(glm::vec3 scale)
-	{
-		scale_ = std::move(scale);
-	}
-
-	/*
-	* @brief Set model rotation.
-	*/
-	void Model::rotate_a(glm::vec4 rotate)
-	{
-		rotate_ = std::move(rotate);
 	}
 	/* @} */
 	/* @@} */
