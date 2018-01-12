c26dfaeeba3f6c6bf027efb859e7d51d8d11d1a4
Stefan Pantic <stefanpantic13@gmail.com>
2018-01-12 22:05:07 +0100

    Added camera get and set options

---
+++ include/opt/lms.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace eRG::opt
+{
+
+	/*
+	* @brief Enum class containing look/move speed setting options. Used for finer camera maniuplations.
+	*/
+	enum class LMS
+	{
+		/* Look/move speed options */
+		LOOK,
+		MOVE,
+	}; /* enum class eRG::opt::LMS */
+
+}; /* namespace eRG::opt */
---
+++ include/opt/point.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace eRG::opt
+{
+
+	/*
+	* @brief Enum class containing stored point options.
+	*/
+	enum class Point
+	{
+		/* Point options */
+		CENTER,
+		EYE,
+		NORMAL,
+	}; /* enum class eRG::opt::Point */
+
+}; /* namespace eRG::opt */
