f01d275d45e3f80bfeb5c982de44cf8b5205e286
Stefan Pantic <stefanpantic13@gmail.com>
2018-02-02 23:50:05 +0100

    Updated convenience script

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# make
+# make (-j tells make to run concurrently)
 make -j
 
 # run program with some default arguments
-exec ./build/find-your-way ./resource/01.json ./resource/texture/wall.bmp ./resource/texture/ceil.bmp ./resource/texture/floor.bmp
+exec ./build/find-your-way ./resource/map/01.json ./resource/texture/wall.bmp ./resource/texture/ceil.bmp ./resource/texture/floor.bmp
